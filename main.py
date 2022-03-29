b73fd383a30911d1447881f0b0f8fdd7acc57096
AliAbdulloh <74522999+AliAbdullo@users.noreply.github.com>
2022-03-29 02:38:51 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# 1 Foydalanivchi kiritgan sonning kvadrati va kubini konsolga chiqaruvchi dastur
+
+son = int(input("Istalgan sonni kiriting:\n>>>"))
+kvadrat = son**2
+kub = son**3
+print(son, " ning kvadrati ", kvadrat, " ga teng")
+print(son, "ning kubi", kub, "ga teng")
+
+# 2 Foydalanuvchidan yoshini so'rab, uning tug'ulgan yilini hisoblab, consolga chiqaruvchi dastur
+#yosh = int(input("Yoshingiz nechida?\n>>>"))
+#print("Siz ",2022-yosh," da tug'ilgansiz")
+
+# 3 Foydalanivchidan ikki son kiritishini sorab, kiritilgan sonning yig'indisi, ayirmasi, ko'paytmasi va bo'linmasi chaqiruvchi dastur
+
+#x = float(input("Birinchi sonni kiriting:"))
+#y = float(input("Ikkinchi sonni kiriting:"))
+#print(f"{x} + {y} =",x+y)
+#print(x,'-', y,'=',x-y)
+#print(x,'*', y,'=',x*y)
+#print(x,'/', y,'=',x/y)
